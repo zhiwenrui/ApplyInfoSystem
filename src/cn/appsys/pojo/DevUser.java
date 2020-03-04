9568e7d4b0bd9d70456e7f2d520a5f43ce96b91d
zhiwenrui <1032325813@qq.com>
2020-03-04 15:26:28 +0800

    wb.xml的配置，资源的导入，用户的创建

---
+++ src/cn/appsys/pojo/DevUser.java
@@ -0,0 +1,16 @@
+package cn.appsys.pojo;
+
+import java.util.Date;
+
+public class DevUser {
+	private int id;   				 //开发者用户id
+	private String devCode; 		 //开发者编码
+	private String devName;          //开发者姓名
+	private String devPassword;      //开发者密码
+	private String devEmail;         //开发者邮箱 
+	private String devInfo;
+	private Integer createdBy;
+	private Date creationDate;
+	private Integer modifuBy;
+	private Date modifyDate;
+}
